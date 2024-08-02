b40611950626f33c878c5dc3082fea7b8e3866ab
Ahmed Zena <87375061+AhmedZena@users.noreply.github.com>
2024-08-02 14:22:46 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors();
+
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
